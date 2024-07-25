3d00d637cf2e15e1f3c101c184527ac3dc441d04
Theetshi <theetshithaldckap@gmail.com>
2024-07-25 10:13:39 +0530

    Path Checking

--- pages/home/script.js
+++ pages/home/script.js
@@ -1,4 +1,4 @@
-import { loadHeader, loadFooter, fetchData } from '../../utility/homeUtility.js';
+import { loadHeader, loadFooter, fetchData } from '../../utility/utility.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     loadHeader();
