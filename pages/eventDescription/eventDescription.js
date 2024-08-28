c39bef9b62a2af4f19852afcb00084c66ac13281
DCKAP <gunasrigdckap@gmail.com>
2024-08-28 09:51:34 +0530

    about and contact page added

--- pages/eventDescription/eventDescription.js
+++ pages/eventDescription/eventDescription.js
@@ -1,297 +1,3 @@
-// import { loadHeader, loadFooter } from '/utility/utility.js';
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     loadHeader();
-//     loadFooter();
-//     setupEventDetails();
-//     setupNavigation();
-//     setupFormSubmission();
-// });
-
-// let isSubmitting = false; // Flag to track form submission state
-
-// function setupEventDetails() {
-//     let urlParams = new URLSearchParams(window.location.search);
-//     let eventId = urlParams.get('id');
-
-//     if (!eventId) {
-//         console.error('Event ID is missing');
-//         return;
-//     }
-
-
-//     // Show loader while fetching data
-//     document.getElementById('descriptionLoader').style.display = 'block';
-
-//     fetch(`http://localhost:8080/api/events?_id=${eventId}`)
-//         .then(response => response.json())
-//         .then(responseData => { 
-//             let event = responseData.data;
-//             if (event) {
-//                 displayEventDetails(event);
-//                 setupModalFunctionality(event);
-//             } else {
-//                 console.error('Event not found');
-//             }
-//         })
-//         .catch(error => console.error('Error fetching data:', error))
-//         .finally(() => {
-//             // Hide loader after data is fetched
-//             document.getElementById('descriptionLoader').style.display = 'none';
-//         });
-// }
-
-// function displayEventDetails(event) {
-//     console.log(event);
-    
-//     document.getElementById('eventImage').src = `/assets/eventsOrgImg/${event.imageUrl}`;
-//     document.getElementById('eventName').textContent = event.eventName;
-//     document.getElementById('eventTagline').textContent = event.tagline;
-//     document.getElementById('eventPrice').textContent = event.price;
-//     document.getElementById('aboutDescription').innerHTML = event.about;
-//     document.getElementById('aboutImage').src = `/assets/eventsOrgImg/${event.about_img}`;
-    
-//     // Projects Section
-//     if (event.imageurl && event.imageurl.length >= 4) {
-//         document.getElementById('projectImg1').src = `/assets/eventsOrgImg/${event.imageurl[0]}`;
-//         document.getElementById('projectImg2').src = `/assets/eventsOrgImg/${event.imageurl[1]}`;
-//         document.getElementById('projectImg3').src = `/assets/eventsOrgImg/${event.imageurl[2]}`;
-//         document.getElementById('projectImg4').src = `/assets/eventsOrgImg/${event.imageurl[3]}`;
-//     } else {
-//         console.error('imageurl array is missing or does not contain enough elements');
-//     }
-
-//     // Contact Section
-//     document.getElementById('contactAddress').innerHTML = `<i class="fas fa-map-marker-alt icon"></i> ${event.address},`;
-//     document.getElementById('contactCity').textContent=`city: ${event.city}.`;
-//     document.getElementById('contactMobile').textContent = `Mobile: ${event.mobile}`;
-//     document.getElementById('contactWhatsapp').textContent = `WhatsApp: ${event.whatsapp}`;
-//     document.getElementById('contactEmail').textContent = event.email;
-// }
-
-// function setupNavigation() {
-//     document.getElementById('aboutSection').addEventListener('click', () => {
-//         document.getElementById('aboutContainer').scrollIntoView({ behavior: 'smooth' });
-//     });
-//     document.getElementById('projectSection').addEventListener('click', () => {
-//         document.getElementById('projectContainer').scrollIntoView({ behavior: 'smooth' });
-//     });
-//     document.getElementById('contactSection').addEventListener('click', () => {
-//         document.getElementById('contactContainer').scrollIntoView({ behavior: 'smooth' });
-//     });
-// }
-
-// function setupModalFunctionality(event) {
-//     let modal = document.getElementById('userModal');
-//     let blurOverlay = document.getElementById('blurOverlay');
-//     let closeBtn = document.querySelector('.close');
-//     let images = event.imageurl || [];
-
-//     document.getElementById('bookingSection').addEventListener('click', () => {
-//         if (images.length > 0) {
-//             let randomImage = images[Math.floor(Math.random() * images.length)];
-//             document.getElementById('randomImage').src = `/assets/eventsOrgImg/${randomImage}`;
-//         } else {
-//             document.getElementById('randomImage').src = '/assets/eventsOrgImg/org1_prewed.jpg'; // Path to a default image
-//         }
-//         openModal(modal, blurOverlay);
-//     });
-
-//     closeBtn.addEventListener('click', () => {
-//         if (!isSubmitting) {
-//             closeModal(modal, blurOverlay);
-//         }
-//     });
-
-//     window.addEventListener('click', (event) => {
-//         if (event.target === blurOverlay && modal.style.display === "flex" && !isSubmitting) {
-//             closeModal(modal, blurOverlay);
-//         }
-//     });
-// }
-
-// function openModal(modal, blurOverlay) {
-//     modal.style.display = "flex";
-//     blurOverlay.style.display = "block";
-//     document.body.style.overflow = "hidden";
-// }
-
-// function closeModal(modal, blurOverlay) {
-//     if (modal.style.display === "flex") {
-//         modal.style.display = "none";
-//         blurOverlay.style.display = "none";
-//         document.body.style.overflow = "auto";
-//         resetForm();
-//     }
-// }
-
-// function resetForm() {
-//     document.getElementById('userForm').reset();
-// }
-
-// function setupFormSubmission() {
-//     let form = document.getElementById('userForm');
-//     form.addEventListener('submit', function(event) {
-//         event.preventDefault();
-//         if (validateForm()) {
-//             let formData = new FormData(form);
-//             let data = {
-//                 name: formData.get('userName'),
-//                 email: formData.get('email'),
-//                 contact: formData.get('contact'),
-//                 address: formData.get('address'),
-//                 organizerEmail: document.getElementById('contactEmail').textContent,
-//                 organizerName: document.getElementById('eventName').textContent,
-//                 organizerAddress: document.getElementById('contactAddress').textContent,
-//                 organizerMobile: document.getElementById('contactMobile').textContent,
-//             };
-
-//             let submitBtn = document.querySelector('.submit-btn');
-
-//             isSubmitting = true; // Set to true when submission starts
-
-//              // Show loader when submitting form
-//              document.getElementById('descriptionLoader').style.display = 'block';
-
-//             if (submitBtn) {
-//                 submitBtn.innerText = "Submitting...";
-//                 submitBtn.disabled = true; // Disable the button while submitting
-//             }
-
-//             fetch(`http://localhost:8080/api/orders`, {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                 },
-//                 body: JSON.stringify(data),
-//             })
-//             .then(response => response.json())
-//             .then(responseData => {
-//                 if (responseData.statusCode === 201 || responseData.success) {
-//                     Swal.fire({
-//                         title: "Success",
-//                         text: "Your order has been placed, and a confirmation email has been sent.",
-//                         icon: "success",
-//                     }).then(() => {
-//                         closeModal(document.getElementById('userModal'), document.getElementById('blurOverlay'));
-//                     });
-//                 } else {
-//                     console.error('Order submission failed:', responseData);
-//                     Swal.fire({
-//                         title: "Error",
-//                         text: "There was an issue placing your order. Please try again.",
-//                         icon: "error",
-//                     });
-//                 }
-//             })
-//             .catch(error => {
-//                 console.error('Error submitting form:', error);
-//                 Swal.fire({
-//                     title: "Error",
-//                     text: "There was a problem with the request. Please try again later.",
-//                     icon: "error",
-//                 });
-//             })
-//             .finally(() => {
-//                 isSubmitting = false; // Reset after submission is complete
-
-//                 // Hide loader after submission is complete
-//                 document.getElementById('descriptionLoader').style.display = 'none';
-                
-//                 if (submitBtn) {
-//                     submitBtn.innerText = "Submit";
-//                     submitBtn.disabled = false; // Re-enable the button
-//                 }
-//             });
-//         }
-//     });
-// }
-
-// function validateForm() {
-//     let isValid = true;
-
-//     let userName = document.getElementById('userName');
-//     let email = document.getElementById('email');
-//     let contact = document.getElementById('contact');
-//     let address = document.getElementById('address');
-//     let agree = document.getElementById('agree');
-
-//     let userNameError = document.getElementById('userNameError');
-//     let emailError = document.getElementById('emailError');
-//     let contactError = document.getElementById('contactError');
-//     let addressError = document.getElementById('addressError');
-//     let agreeError = document.getElementById('agreeError');
-
-//     let errors = [];
-
-//     if (userName.value.trim() === "") {
-//         userNameError.textContent = "Enter user name";
-//         userNameError.style.display = 'block';
-//         errors.push(userNameError);
-//         isValid = false;
-//     } else {
-//         userNameError.style.display = 'none';
-//     }
-
-//     let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     if (email.value.trim() === "") {
-//         emailError.textContent = "Enter email";
-//         emailError.style.display = 'block';
-//         errors.push(emailError);
-//         isValid = false;
-//     } else if (!emailPattern.test(email.value.trim())) {
-//         emailError.textContent = "Enter valid email";
-//         emailError.style.display = 'block';
-//         errors.push(emailError);
-//         isValid = false;
-//     } else {
-//         emailError.style.display = 'none';
-//     }
-
-//     let contactPattern = /^[6-9]\d{9}$/;
-//     if (contact.value.trim() === "") {
-//         contactError.textContent = "Enter contact number";
-//         contactError.style.display = 'block';
-//         errors.push(contactError);
-//         isValid = false;
-//     } else if (!contactPattern.test(contact.value.trim())) {
-//         contactError.textContent = "Invalid contact number";
-//         contactError.style.display = 'block';
-//         errors.push(contactError);
-//         isValid = false;
-//     } else {
-//         contactError.style.display = 'none';
-//     }
-
-//     if (address.value.trim() === "") {
-//         addressError.textContent = "Enter address";
-//         addressError.style.display = 'block';
-//         errors.push(addressError);
-//         isValid = false;
-//     } else {
-//         addressError.style.display = 'none';
-//     }
-
-//     if (!agree.checked) {
-//         agreeError.textContent = "You must agree to the terms";
-//         agreeError.style.display = 'block';
-//         errors.push(agreeError);
-//         isValid = false;
-//     } else {
-//         agreeError.style.display = 'none';
-//     }
-
-//     if (errors.length > 0) {
-//         setTimeout(() => {
-//             errors.forEach(error => error.style.display = 'none');
-//         }, 3000);
-//     }
-
-//     return isValid;
-// }
-
-
-
 import { loadHeader, loadFooter } from '/utility/utility.js';
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -394,14 +100,24 @@ function setupModalFunctionality(event) {
     let blurOverlay = document.getElementById('blurOverlay');
     let closeBtn = document.querySelector('.close');
     let images = event.imageurl || [];
+    let fallbackImageUrl = "http://localhost:8080/" + event.imageUrl.slice(7); // Fallback image
+    let dummyImageUrl = '/assets/eventsImages/dance.jpg'; // if image not found then show this image
 
     document.getElementById('bookingSection').addEventListener('click', () => {
-        if (images.length > 0) {
-            let randomImage = images[Math.floor(Math.random() * images.length)];
-            document.getElementById('randomImage').src = `/assets/eventsOrgImg/${randomImage}`;
+        let randomImage = images.length > 0 ? images[Math.floor(Math.random() * images.length)] : null;
+        let randomImageElement = document.getElementById('randomImage');
+
+        if (randomImage) {
+            let primaryImageUrl = `/assets/eventsOrgImg/${randomImage}`;
+            randomImageElement.src = primaryImageUrl;
+
+            randomImageElement.onerror = () => {
+                randomImageElement.src = fallbackImageUrl;
+            };
         } else {
-            document.getElementById('randomImage').src = '/assets/eventsOrgImg/org1_prewed.jpg'; // Path to a default image
+            randomImageElement.src = dummyImageUrl;
         }
+
         openModal(modal, blurOverlay);
     });
 
@@ -417,6 +133,8 @@ function setupModalFunctionality(event) {
         }
     });
 }
+
+
 
 function openModal(modal, blurOverlay) {
     modal.style.display = "flex";
--- pages/eventOrganizationPage/eventOrganizationPage.js
+++ pages/eventOrganizationPage/eventOrganizationPage.js
@@ -1,107 +1,221 @@
 
 import { loadHeader, loadFooter } from '/utility/utility.js';
 
-document.addEventListener("DOMContentLoaded", () => {console.log("hello")
+
+document.addEventListener("DOMContentLoaded", () => {
 
     loadHeader();
     loadFooter();
-})
+});
+
 document.getElementById('eventForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    // Create a new FormData object to hold the form data
-    const formData = new FormData(this);
+    let isValid = true;
 
-    try {
-        const response = await fetch('http://localhost:8080/api/events/eventCreate', {
-            method: 'POST',
-            body: formData
-        });
+    // Function to hide error messages after 3 seconds
+    const hideErrorAfterDelay = (element) => {
+        setTimeout(() => {
+            element.textContent = "";
+        }, 2000);
+    };
 
-        if (response.ok) {
-            const jsonResponse = await response.json();
-            console.log('Event created successfully:', jsonResponse);
-            Swal.fire({
-                title: "Success",
-                text: "Your order has been placed, and a confirmation email has been sent.",
-                icon: "success",
-                })
-                // Clear the form fields after successful submission
-                document.querySelector('form').reset();
-        } else {
-            const errorResponse = await response.json();
-            console.error('Error creating event:', errorResponse);
+    // Organization Name Validation
+    const organizationName = document.getElementById('organizationname');
+    const organizationNameError = document.getElementById('organizationname-error');
+    if (organizationName.value.trim() === "") {
+        organizationNameError.textContent = "Organization name is required.";
+        isValid = false;
+        hideErrorAfterDelay(organizationNameError);
+    } else {
+        organizationNameError.textContent = "";
+    }
+
+    // Event Name Validation
+    const eventName = document.getElementById('eventName');
+    const eventNameError = document.getElementById('eventName-error');
+    if (eventName.value.trim() === "") {
+        eventNameError.textContent = "Event name is required.";
+        isValid = false;
+        hideErrorAfterDelay(eventNameError);
+    } else {
+        eventNameError.textContent = "";
+    }
+
+    // Tagline Validation
+    const tagline = document.getElementById('tagline');
+    const taglineError = document.getElementById('tagline-error');
+    if (tagline.value.trim() === "") {
+        taglineError.textContent = "Event tagline is required.";
+        isValid = false;
+        hideErrorAfterDelay(taglineError);
+    } else {
+        taglineError.textContent = "";
+    }
+
+    // Email Validation
+    const email = document.getElementById('email');
+    const emailError = document.getElementById('email-error');
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email.value.trim())) {
+        emailError.textContent = "Please enter a valid email address.";
+        isValid = false;
+        hideErrorAfterDelay(emailError);
+    } else {
+        emailError.textContent = "";
+    }
+
+    // Mobile Number Validation
+    const mobile = document.getElementById('mobile');
+    const mobileError = document.getElementById('mobile-error');
+    const mobilePattern = /^[6-9]\d{9}$/;
+    if (!mobilePattern.test(mobile.value.trim())) {
+        mobileError.textContent = "Please enter a valid 10-digit mobile number.";
+        isValid = false;
+        hideErrorAfterDelay(mobileError);
+    } else {
+        mobileError.textContent = "";
+    }
+
+    // WhatsApp Number Validation
+    const whatsapp = document.getElementById('whatsapp');
+    const whatsappError = document.getElementById('whatsapp-error');
+    if (!mobilePattern.test(whatsapp.value.trim())) {
+        whatsappError.textContent = "Please enter a valid 10-digit WhatsApp number.";
+        isValid = false;
+        hideErrorAfterDelay(whatsappError);
+    } else {
+        whatsappError.textContent = "";
+    }
+
+    // Price Validation
+    const price = document.getElementById('price');
+    const priceError = document.getElementById('price-error');
+    if (price.value.trim() === "" || isNaN(price.value) || Number(price.value) <= 0) {
+        priceError.textContent = "Please enter a valid price.";
+        isValid = false;
+        hideErrorAfterDelay(priceError);
+    } else {
+        priceError.textContent = "";
+    }
+
+    // Category Validation
+    const category = document.getElementById('category');
+    const categoryError = document.getElementById('category-error');
+    if (category.value === "") {
+        categoryError.textContent = "Please select an event category.";
+        isValid = false;
+        hideErrorAfterDelay(categoryError);
+    } else {
+        categoryError.textContent = "";
+    }
+
+    // City Validation
+    const city = document.getElementById('city');
+    const cityError = document.getElementById('city-error');
+    if (city.value === "") {
+        cityError.textContent = "Please select a city.";
+        isValid = false;
+        hideErrorAfterDelay(cityError);
+    } else {
+        cityError.textContent = "";
+    }
+
+    // About Validation
+    const about = document.getElementById('about');
+    const aboutError = document.getElementById('about-error');
+    if (about.value.trim() === "" || about.value.trim().length < 10) {
+        aboutError.textContent = "Please enter at least 10 characters.";
+        isValid = false;
+        hideErrorAfterDelay(aboutError);
+    } else {
+        aboutError.textContent = "";
+    }
+
+    // Event Description Validation
+    const eventDescription = document.getElementById('eventDescription');
+    const eventDescriptionError = document.getElementById('eventDescription-error');
+    if (eventDescription.value.trim() === "" || eventDescription.value.trim().length < 10) {
+        eventDescriptionError.textContent = "Please enter at least 10 characters.";
+        isValid = false;
+        hideErrorAfterDelay(eventDescriptionError);
+    } else {
+        eventDescriptionError.textContent = "";
+    }
+
+    // Address Validation
+    const address = document.getElementById('address');
+    const addressError = document.getElementById('address-error');
+    if (address.value.trim() === "") {
+        addressError.textContent = "Please enter an address.";
+        isValid = false;
+        hideErrorAfterDelay(addressError);
+    } else {
+        addressError.textContent = "";
+    }
+
+    // Event Image Validation
+    const eventImage = document.getElementById('eventImage');
+    const eventImageError = document.getElementById('eventImage-error');
+    if (eventImage.files.length === 0) {
+        eventImageError.textContent = "Please upload an event image.";
+        isValid = false;
+        hideErrorAfterDelay(eventImageError);
+    } else {
+        eventImageError.textContent = "";
+    }
+
+    // Project Images Validation
+    const projectImages = document.getElementById('projectImages');
+    const projectImagesError = document.getElementById('projectImages-error');
+    if (projectImages.files.length === 0 || projectImages.files.length > 4) {
+        projectImagesError.textContent = "Please upload between 1 and 4 project images.";
+        isValid = false;
+        hideErrorAfterDelay(projectImagesError);
+    } else {
+        projectImagesError.textContent = "";
+    }
+
+    if (isValid) {
+        // Create a new FormData object to hold the form data
+        const formData = new FormData(this);
+
+        try {
+            const response = await fetch('http://localhost:8080/api/events/eventCreate', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                console.log('Event created successfully:', jsonResponse);
+                Swal.fire({
+                    title: "Success",
+                    text: "Your event has been created successfully and a confirmation email has been sent.",
+                    icon: "success",
+                }).then(() => {
+                    // Clear the form fields after successful submission
+                    document.querySelector('form').reset();
+                    // Redirect to the organizer page
+                    window.location.href = '/pages/organiserPage/organiser.html';
+                });
+            } else {
+                const errorResponse = await response.json();
+                console.error('Error creating event:', errorResponse);
+                Swal.fire({
+                    title: "Error",
+                    text: "There was an issue creating your event. Please try again.",
+                    icon: "error",
+                });
+            }
+        } catch (error) {
+            console.error('Network error:', error);
             Swal.fire({
                 title: "Error",
-                text: "There was an issue placing your order. Please try again.",
+                text: "There was a network error. Please try again later.",
                 icon: "error",
             });
-            // alert('Failed to create event. Please try again.');
         }
-    } catch (error) {
-        console.error('Network error:', error);
-        Swal.fire({
-            title: "Error",
-            text: "There was an issue placing your order. Please try again.",
-            icon: "error",
-        });
-        // alert('Network error. Please try again later.');
     }
 });
-// document.querySelector('form').addEventListener('submit', async function(e) {
-     
-//     e.preventDefault();
-    
-//     let formData = {
-//         organizationname: document.getElementById('organization').value,
-//         eventName: document.getElementById('eventname').value,
-//         eventDescription: document.getElementById('eventdescription').value,
-//         price: document.getElementById('eventprice').value,
-//         city: document.getElementById('selectcity').value,
-//         tagline: document.getElementById('eventtagline').value,
-//         // about_img: document.getElementById('eventimage').files[0],
-//         about: document.getElementById('eventabout').value,
-//         // imageurl: Array.from(document.getElementById('projectimages').files),
-//         address: document.getElementById('address').value,
-//         whatsapp: document.getElementById('whatsapp').value,
-//         mobile: document.getElementById('mobile').value,
-//         category: document.getElementById('selecteventcategory').value,
-//         email: document.getElementById('email').value,
-//     };
 
-//     console.log("Form Data:", formData);
-
-//     try {
-//         const response = await fetch('http://localhost:8080/api/events/createEvent', {
-//             method: 'POST',
-//             body: JSON.stringify(formData),
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         });
-
-//         if (response.ok) {
-//             let result = await response.json();
-//             console.log("Success:", result);
-//             Swal.fire({
-//                 title: "Success",
-//                 text: "Your order has been placed, and a confirmation email has been sent.",
-//                 icon: "success",
-//             })
-
-            // // Clear the form fields after successful submission
-            // document.querySelector('form').reset();
-
-//         } else {
-//             let error = await response.json();
-//             console.log("Error:", error);
-            // Swal.fire({
-            //     title: "Error",
-            //     text: "There was an issue placing your order. Please try again.",
-            //     icon: "error",
-            // });
-//         }
-//     } catch (error) {
-//         console.error("Request Failed:", error);
-//     }
-// });
